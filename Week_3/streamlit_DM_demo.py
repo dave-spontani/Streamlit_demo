88850c94b30ff35173f525e7621d08169b7fa65d
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-11 10:21:45 +0200

    Add files via upload

---
+++ Week_3/streamlit_DM_demo.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.title("Here is our first Web Application")
+st.header("Let's roll the dice :sunglasses: ", divider='rainbow')
+st.write("Look how esily we can build and tun a web application")
+st.write("And by the way, list comprehensions are superior")
+
