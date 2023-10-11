560a7075eb1a1177f696fe5cfa69e92ae3cdf049
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-11 10:26:13 +0200

    Update

--- Week_3/streamlit_DM_demo.py
+++ Week_3/streamlit_DM_demo.py
@@ -5,4 +5,5 @@ st.title("Here is our first Web Application")
 st.header("Let's roll the dice :sunglasses: ", divider='rainbow')
 st.write("Look how esily we can build and tun a web application")
 st.write("And by the way, list comprehensions are superior")
+st.write("And furthermore, this way of doing streamlit is great")
 
